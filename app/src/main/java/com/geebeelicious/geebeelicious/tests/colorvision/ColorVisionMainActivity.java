67cfc8c7eba2b2d384f71fd299fe99059b92b5cd
katelacsamana <katelacsamana@gmail.com>
2016-03-04 17:15:22 +0800

    Added resource images for Ishihara test
    Added custom Ishihara plates for the color vision test

---
+++ app/src/main/java/com/geebeelicious/geebeelicious/tests/colorvision/ColorVisionMainActivity.java
@@ -0,0 +1,16 @@
+package com.geebeelicious.geebeelicious.tests.colorvision;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.geebeelicious.geebeelicious.R;
+
+public class ColorVisionMainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_vision_main);
+    }
+
+}

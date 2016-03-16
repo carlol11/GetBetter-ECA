d0c876cf5b557503de13ab70c836105f895355ba
mgmalana <mgmalana@MGs-MacBook-Pro.local>
2016-03-17 02:22:56 +0800

    Added the test flow. Now logs the results
    Test Flow:
    1.Test Non-dominant hand with finger
    2.Test dominant hand with pen
    3.Ask assistant (yes or no) if child was able to use pen without
    difficulties

--- app/src/main/java/com/geebeelicious/geebeelicious/tests/finemotor/FineMotorsActivity.java
+++ app/src/main/java/com/geebeelicious/geebeelicious/tests/finemotor/FineMotorsActivity.java
@@ -7,10 +7,14 @@ import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.geebeelicious.geebeelicious.R;
@@ -20,29 +24,75 @@ import com.geebeelicious.geebeelicious.R;
 * Activity for the Fine Motors Test
 */
 public class FineMotorsActivity extends Activity {
+
+    private static final String TAG = "FineMotorsActivity";
     //Set the color for the start and end of the path
-    private static final int START_COLOR = Color.WHITE;
-    private static final int END_COLOR = Color.BLACK;
+    private static final int START_COLOR = Color.WHITE; //update the instruction if you change this
+    private static final int END_COLOR = Color.BLACK;  //update the instruction if you change this
 
     private ImageView imageViewPathToTrace;
     private TextView ECAtext;
     private MediaPlayer mp;
+    private Button buttonYes;
+    private Button buttonNo;
+
+    private boolean isDominantHand = false; //current test the user is taking
+    private boolean isTestOngoing = false;
+    private boolean[] result = new boolean[3]; //result[i] is true if pass, false if fail
+
+    //TODO: Change instructions to be more specific. since you know the dominant hand na. also the gender
+    private String[] instructions = {"Using a finger of your non dominant hand, trace the path. Start from the white circle and go to the black circle",
+        "Using the pen with your dominant hand, trace the path. Start from the white circle and go to the black circle",
+        "Assistant, has he/she used the pen without difficulties?"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fine_motors);
+        setContentView(R.layout.activity_fine_motor);
+
         ECAtext = (TextView) findViewById(R.id.placeholderECAText);
         imageViewPathToTrace = (ImageView) findViewById(R.id.imageViewPathToTrace);
         mp = MediaPlayer.create(getApplicationContext(), R.raw.fine_motor_outside_path);
+        buttonYes = (Button) findViewById(R.id.YesButton);
+        buttonNo = (Button) findViewById(R.id.NoButton);
 
         mp.setLooping(true);
         imageViewPathToTrace.setOnTouchListener(image_Listener);
+        ECAtext.setText(instructions[0]);
+        buttonYes.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result[2] = true;
+                sendResults();
+            }
+        });
+
+        buttonNo.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result[2] = false;
+                sendResults();
+            }
+        });
+
+    }
+
+    //TODO: Intent shound be sent through this method
+    private synchronized void sendResults(){
+        if(isTestOngoing){ //this is too avoid double clicking
+            for(boolean b: result){
+                String a = (b ? "pass" : "fail");
+                Log.d(TAG, "Results: " + (b ? "pass" : "fail"));
+            }
+            isTestOngoing = false;
+        }
     }
 
     private OnTouchListener image_Listener = new OnTouchListener(){
-        private boolean haveStarted = false; //has user started
+        private boolean hasStarted = false; //has user started
         private boolean wasOutside = false; //was user outside the path
+        private int numWrongs = 0;
 
         @Override
         public boolean onTouch(View v, MotionEvent event) {
@@ -52,23 +102,28 @@ public class FineMotorsActivity extends Activity {
             int[] xY = getBitMapCoordinates(bitmap, event.getX(), event.getY());
             int pixel = bitmap.getPixel(xY[0], xY[1]);
 
-            if(haveStarted){ //if user have pressed start_color
+            if(hasStarted){ //if user have pressed start_color
                 switch(event.getAction()){
                     case MotionEvent.ACTION_DOWN:
-                        return true;
+                        break;
+                    case MotionEvent.ACTION_UP: //go back to start if finger is lifted
+                        doIfTouchIsUp();
+                        break;
                     case MotionEvent.ACTION_MOVE:
                         if(pixel == END_COLOR) { //if user done
-                            doFinishPathSuccess();
-                        } else {
-                            if(pixel == 0) { //if outside the lines
-                                doIfOutSideThePath();
-                            } else if (wasOutside){
+                            doFinishPathSuccess(isDominantHand);
+                        } else if(pixel == 0){ //if touch is outside path
+                            doIfOutSideThePath();
+                        } else { //if touch is within path
+                            if (wasOutside){
                                 mp.pause();
                                 wasOutside = false;
+                            } else {
+                                ECAtext.setText("Trace to black circle");
                             }
 
-                            ECAtext.setText("Touch event position: " + eventX + ", " + eventY + "\n" +
-                                    "Pixel: " + pixel);
+                            Log.d(TAG, "Touch event position: " + eventX + ", " + eventY + "\n" +
+                                    "Pixel: " + pixel + " Number of wrongs: " + numWrongs);
                         }
                         break;
                     default:
@@ -78,7 +133,7 @@ public class FineMotorsActivity extends Activity {
             } else {
 
                 if(pixel == START_COLOR && event.getAction() == MotionEvent.ACTION_DOWN){
-                    haveStarted = true;
+                    hasStarted = true;
                     return true;
                 }
                 return false;
@@ -87,8 +142,14 @@ public class FineMotorsActivity extends Activity {
         }
 
         //called if user finished path successfully
-        private void doFinishPathSuccess(){
-            ECAtext.setText("You've finished the path successfully!");
+        private synchronized void doFinishPathSuccess(boolean isDominant){
+            if(!isDominant && hasStarted){
+                ECAtext.setText("You've finished the path successfully!");
+                doTestWithPen();
+            } else if(isDominant && hasStarted) {
+                ECAtext.setText("You've finished the path successfully!");
+                askAssistantOfPen();
+            }
         }
 
         //called if the user is outside the path
@@ -96,9 +157,35 @@ public class FineMotorsActivity extends Activity {
             if(!wasOutside){
                 mp.start();
                 wasOutside = true;
+                numWrongs++;
             }
         }
-        
+
+        //called if the user lifts the touch while doing the path. user must go back to start
+        private void doIfTouchIsUp(){
+            hasStarted = false;
+            ECAtext.setText("Don't lift your finger. Go back to start.");
+            mp.pause();
+        }
+
+        //starts the 2nd test. resets the variables
+        private void doTestWithPen(){
+            hasStarted = false;
+            result[0] = numWrongs < 2;
+            numWrongs = 0;
+            ECAtext.setText(instructions[1]);
+            isDominantHand = true;
+        }
+
+        //asks the assistant if the user was able to use the pen properly. saves the results too
+        private void askAssistantOfPen(){
+            LinearLayout linearLayoutAnswer = (LinearLayout) findViewById(R.id.linearLayoutAnswers);
+            hasStarted = false;
+            result[1] = numWrongs < 2;
+            ECAtext.setText(instructions[2]);
+            linearLayoutAnswer.setVisibility(View.VISIBLE);
+        }
+
         //returns the equivalent x and y coordinates of the bitmap given x and y coordinates of the touch event
         private int[] getBitMapCoordinates(Bitmap bitmap, float eventX, float eventY){
             Matrix invertMatrix = new Matrix();
@@ -108,8 +195,8 @@ public class FineMotorsActivity extends Activity {
 
             ((ImageView)imageViewPathToTrace).getImageMatrix().invert(invertMatrix);
             invertMatrix.mapPoints(eventXY);
-            x = Integer.valueOf((int) eventXY[0]);
-            y = Integer.valueOf((int) eventXY[1]);
+            x = (int) eventXY[0];
+            y = (int) eventXY[1];
 
             if(x < 0){
                 x = 0;

9194feb0460f105dd9c027bcb71d208c76f78ccf
katelacsamana <katelacsamana@gmail.com>
2016-03-30 04:17:36 +0800

    Fixed GUI to allow Spinner to display schools

--- app/src/main/java/com/geebeelicious/geebeelicious/SettingsActivity.java
+++ app/src/main/java/com/geebeelicious/geebeelicious/SettingsActivity.java
@@ -7,11 +7,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.geebeelicious.geebeelicious.adapters.SchoolsAdapter;
+
 import java.util.ArrayList;
 
 import models.consultation.School;
 
 public class SettingsActivity extends ActionBarActivity {
+
+    private School chosenSchool = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +23,18 @@ public class SettingsActivity extends ActionBarActivity {
         setContentView(R.layout.activity_settings);
 
         //TODO: [URGENT, DB] Get list of schools from DB
-        ArrayList<School> schools = new ArrayList<School>();
+        final ArrayList<School> schools = new ArrayList<School>();
         schools.add(new School(1, "Hammy School of the Arts", "Los Angeles"));
         schools.add(new School(2, "East High School", "Albuquerque"));
 
+        SchoolsAdapter schoolsAdapter = new SchoolsAdapter(SettingsActivity.this, schools);
         Spinner schoolSpinner = (Spinner)findViewById(R.id.schoolSpinner);
+        schoolSpinner.setAdapter(schoolsAdapter);
         //TODO: [URGENT, DB] Set school to whatever is selected when save button is clicked
         schoolSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                chosenSchool = schools.get(position);
             }
 
             @Override

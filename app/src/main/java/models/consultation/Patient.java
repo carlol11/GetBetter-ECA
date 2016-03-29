b839e2836d0a25c9e6a1efcb6790b677cae7f1fa
katelacsamana <katelacsamana@gmail.com>
2016-03-30 03:09:39 +0800

    Added method to return gender in String format

--- app/src/main/java/models/consultation/Patient.java
+++ app/src/main/java/models/consultation/Patient.java
@@ -121,4 +121,12 @@ public class Patient implements Parcelable {
     public int getHandedness() {
         return handedness;
     }
+
+    public String getGenderString(){
+        if(gender == 0){
+            return "Male";
+        } else{
+            return "Female";
+        }
+    }
 }

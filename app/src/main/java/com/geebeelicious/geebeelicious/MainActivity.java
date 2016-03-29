91948806c7307d1ee4803fe452760b39581486fd
katelacsamana <katelacsamana@gmail.com>
2016-03-30 03:09:16 +0800

    Fixed MainActivity to have functionality for main menu (launch able)

--- app/src/main/java/com/geebeelicious/geebeelicious/MainActivity.java
+++ app/src/main/java/com/geebeelicious/geebeelicious/MainActivity.java
@@ -1,9 +1,12 @@
 package com.geebeelicious.geebeelicious;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +14,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO: [UI for Kate] Make logo for main activity
+        //TODO: Make Settings Activity for Hearing Test Calibration and School List
+        Button startButton = (Button)findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PatientListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override

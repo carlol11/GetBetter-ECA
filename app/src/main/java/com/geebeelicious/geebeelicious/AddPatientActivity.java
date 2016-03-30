6a1f96702094b5624f0135f211b913728351d7e5
katelacsamana <katelacsamana@gmail.com>
2016-03-31 00:59:28 +0800

    Edited AddPatientActivity to handle changing views

--- app/src/main/java/com/geebeelicious/geebeelicious/AddPatientActivity.java
+++ app/src/main/java/com/geebeelicious/geebeelicious/AddPatientActivity.java
@@ -2,6 +2,8 @@ package com.geebeelicious.geebeelicious;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -11,40 +13,86 @@ import models.consultation.Patient;
 
 public class AddPatientActivity extends ActionBarActivity {
 
+    private String firstName = null;
+    private String lastName = null;
+    private String birthDate = null;
+    private String gender = null;
+    private String handedness = null;
+    private TextView questionView;
+    private EditText editText;
+    private RadioGroup radioGroup;
+    private DatePicker datePicker;
+    private int questionCounter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_patient);
 
-        String firstName = null;
-        String lastName = null;
-        String birthDate = null;
-        String gender = null;
-        String handedness = null;
+        questionView = (TextView)findViewById(R.id.questionView);
+        editText = (EditText)findViewById(R.id.newPatientStringInput);
+        datePicker = (DatePicker)findViewById(R.id.newPatientDatePicker);
+        radioGroup = (RadioGroup)findViewById(R.id.radio);
 
-        setQuestion(R.string.first_name);
+        int[] questions = {R.string.first_name, R.string.last_name, R.string.birthdate,
+                            R.string.gender, R.string.handedness};
+
+        setQuestion(questions[questionCounter]);
+        editText.setVisibility(View.VISIBLE);
+
+        Button saveButton = (Button)findViewById(R.id.saveNewPatientButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(questionCounter){
+                    case 0:
+                        firstName = getEditText();
+                        break;
+                    case 1:
+                        lastName = getEditText();
+                        editText.setVisibility(View.GONE);
+                        datePicker.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        birthDate = getSelectedDate();
+                        datePicker.setVisibility(View.GONE);
+                        radioGroup.setVisibility(View.VISIBLE);
+                        break;
+                    case 3:
+                        gender = getRadioResult();
+                        break;
+                    case 4:
+                        handedness = getRadioResult();
+                        radioGroup.setVisibility(View.GONE);
+                        editText.setVisibility(View.VISIBLE);
+                        break;
+                    default:
+                        firstName = getEditText();
+                        break;
+                }
+                questionCounter++;
+                if(questionCounter>4){
+                    //TODO: [URGENT, DB] write to database
+                }
+            }
+        });
 
     }
 
     private void setQuestion(int resID){
-        TextView questionView = (TextView)findViewById(R.id.questionView);
         questionView.setText(resID);
     };
 
     private String getEditText(){
-        EditText editText = (EditText)findViewById(R.id.newPatientStringInput);
         return editText.getText().toString();
     };
 
     private String getSelectedDate(){
-        DatePicker datePicker = (DatePicker)findViewById(R.id.newPatientDatePicker);
         return (datePicker.getMonth() + "/" + datePicker.getDayOfMonth() + "/" + datePicker.getYear());
     }
 
     private String getRadioResult(){
-        RadioGroup radioGroup = (RadioGroup)findViewById(R.id.radio);
         return String.valueOf(radioGroup.getCheckedRadioButtonId());
-
     }
 
 }

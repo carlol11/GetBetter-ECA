a537018b0d3f6da12f521651b2cf3fcace958c21
katelacsamana <katelacsamana@gmail.com>
2016-07-10 19:59:03 +0800

    Added Tumbling E chart as option

--- app/src/main/java/com/geebeelicious/geebeelicious/models/visualacuity/ChartHelper.java
+++ app/src/main/java/com/geebeelicious/geebeelicious/models/visualacuity/ChartHelper.java
@@ -4,7 +4,6 @@ import android.widget.ImageView;
 
 import com.geebeelicious.geebeelicious.R;
 
-
 /**
  * Created by Kate on 02/24/2016.
  * The ChartHelper class manages the
@@ -21,22 +20,40 @@ public class ChartHelper {
     private boolean isRightTested;
     private boolean isLeftTested;
 
-    public ChartHelper(ImageView chartView){
+    public ChartHelper(ImageView chartView, int chartPreference){
         chart = new ChartLine[11];
-        chart[0] = new ChartLine(1, 20, 200, R.drawable.snellen_line_1);
-        chart[1] = new ChartLine(2, 20, 100, R.drawable.snellen_line_2);
-        chart[2] = new ChartLine(3, 20, 70, R.drawable.snellen_line_3);
-        chart[3] = new ChartLine(4, 20, 50, R.drawable.snellen_line_4);
-        chart[4] = new ChartLine(5, 20, 40, R.drawable.snellen_line_5);
-        chart[5] = new ChartLine(6, 20, 30, R.drawable.snellen_line_6);
-        chart[6] = new ChartLine(7, 20, 25, R.drawable.snellen_line_7);
-        chart[7] = new ChartLine(8, 20, 20, R.drawable.snellen_line_8);
-        chart[8] = new ChartLine(9, 20, 15, R.drawable.snellen_line_9);
-        chart[9] = new ChartLine(10, 20, 10, R.drawable.snellen_line_10);
-        chart[10] = new ChartLine(11, 20, 5, R.drawable.snellen_line_11);
+        initializeChartPreference(chartPreference);
         this.chartView = chartView;
         this.isRightTested = false;
         this.isLeftTested = false;
+    }
+
+    private void initializeChartPreference(int chartPreference){
+        if(chartPreference == 1){ //Tumbling E Chart
+            chart[0] = new ChartLine(1, 20, 200, R.drawable.tumbling_e_line_1);
+            chart[1] = new ChartLine(2, 20, 100, R.drawable.tumbling_e_line_2);
+            chart[2] = new ChartLine(3, 20, 70, R.drawable.tumbling_e_line_3);
+            chart[3] = new ChartLine(4, 20, 50, R.drawable.tumbling_e_line_4);
+            chart[4] = new ChartLine(5, 20, 40, R.drawable.tumbling_e_line_5);
+            chart[5] = new ChartLine(6, 20, 30, R.drawable.tumbling_e_line_6);
+            chart[6] = new ChartLine(7, 20, 25, R.drawable.tumbling_e_line_7);
+            chart[7] = new ChartLine(8, 20, 20, R.drawable.tumbling_e_line_8);
+            chart[8] = new ChartLine(9, 20, 15, R.drawable.tumbling_e_line_9);
+            chart[9] = new ChartLine(10, 20, 10, R.drawable.tumbling_e_line_10);
+            chart[10] = new ChartLine(11, 20, 5, R.drawable.tumbling_e_line_11);
+        } else { //Snellen Chart
+            chart[0] = new ChartLine(1, 20, 200, R.drawable.snellen_line_1);
+            chart[1] = new ChartLine(2, 20, 100, R.drawable.snellen_line_2);
+            chart[2] = new ChartLine(3, 20, 70, R.drawable.snellen_line_3);
+            chart[3] = new ChartLine(4, 20, 50, R.drawable.snellen_line_4);
+            chart[4] = new ChartLine(5, 20, 40, R.drawable.snellen_line_5);
+            chart[5] = new ChartLine(6, 20, 30, R.drawable.snellen_line_6);
+            chart[6] = new ChartLine(7, 20, 25, R.drawable.snellen_line_7);
+            chart[7] = new ChartLine(8, 20, 20, R.drawable.snellen_line_8);
+            chart[8] = new ChartLine(9, 20, 15, R.drawable.snellen_line_9);
+            chart[9] = new ChartLine(10, 20, 10, R.drawable.snellen_line_10);
+            chart[10] = new ChartLine(11, 20, 5, R.drawable.snellen_line_11);
+        }
     }
 
     private ChartLine getPreviousLine(){

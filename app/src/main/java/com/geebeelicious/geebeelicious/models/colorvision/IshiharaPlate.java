74cc1cd1ef0a485cc9d51786a9489e0f713299ab
mgmalana <mgmalana@gmail.com>
2016-08-21 22:20:05 +0800

    IshiharaPlate Javadoc

--- app/src/main/java/com/geebeelicious/geebeelicious/models/colorvision/IshiharaPlate.java
+++ app/src/main/java/com/geebeelicious/geebeelicious/models/colorvision/IshiharaPlate.java
@@ -1,20 +1,45 @@
 package com.geebeelicious.geebeelicious.models.colorvision;
 
 /**
- * Created by Kate on 03/04/2016.
  * The IshiharaPlate represents a plate
  * to be used in the test. The class
  * represents the plate through information
  * about its shape, its style, and its
  * corresponding drawable.
+ *
+ * @author Katrina Lacsamana
+ * @since 03/04/2016
  */
 public class IshiharaPlate {
 
+    /**
+     * Shape inside the plate.
+     */
     private String shape;
+
+    /**
+     * Type of Ishihara plate. Types are depending
+     * on the color combination of the plate.
+     */
     private int style;
+
+    /**
+     * Drawable of the ishihira plate.
+     */
     private int ishiharaPlateDrawable;
+
+    /**
+     * Flag to show whether it has already been added
+     * in the generated test. True if added, false if not.
+     */
     private boolean isAdded;
 
+    /**
+     * Constructor.
+     * @param shape {@link IshiharaPlate#shape}
+     * @param style {@link IshiharaPlate#style}
+     * @param ishiharaPlateDrawable {@link IshiharaPlate#ishiharaPlateDrawable}
+     */
     public IshiharaPlate(String shape, int style, int ishiharaPlateDrawable){
         this.shape = shape;
         this.style = style;
@@ -22,22 +47,42 @@ public class IshiharaPlate {
         this.isAdded = false;
     }
 
+    /**
+     * Gets {@link #shape}
+     * @return {@link #shape}
+     */
     public String getShape() {
         return shape;
     }
 
+    /**
+     * Gets {@link #style}
+     * @return {@link #style}
+     */
     public int getStyle() {
         return style;
     }
 
+    /**
+     * Gets {@link #ishiharaPlateDrawable}
+     * @return {@link #ishiharaPlateDrawable}
+     */
     public int getIshiharaPlateDrawable() {
         return ishiharaPlateDrawable;
     }
 
+    /**
+     * Gets {@link #isAdded}
+     * @return {@link #isAdded}
+     */
     public boolean isAdded() {
         return isAdded;
     }
 
+    /**
+     * Gets {@link #setAdded}
+     * @return {@link #setAdded}
+     */
     public void setAdded(){
         isAdded = true;
     }

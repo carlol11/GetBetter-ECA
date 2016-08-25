0227a9fc97a5b86dc38391044fa8c586183c3529
mgmalana <mgmalana@gmail.com>
2016-08-25 21:51:14 +0800

    Impressions Javadoc

--- app/src/main/java/com/geebeelicious/geebeelicious/models/consultation/Impressions.java
+++ app/src/main/java/com/geebeelicious/geebeelicious/models/consultation/Impressions.java
@@ -5,16 +5,62 @@ import java.util.ArrayList;
 /**
  * GetBetter. Created by Mike Dayupay on 9/21/15.
  */
+
+/**
+ * The Impressions class represents the impressions to be
+ * tested given the chief complaint. This is mainly used by
+ * the {@link ExpertSystem}.
+ *
+ * @see ChiefComplaint
+ * @see Symptom
+ * @see SymptomFamily
+ */
 public class Impressions {
 
+    /**
+     * ID of the impression
+     */
     private int impressionId;
+
+    /**
+     * Impression name
+     */
     private String impression;
+
+    /**
+     * Scientific name of the impression
+     */
     private String scientificName;
+
+    /**
+     * Local name of the impression
+     */
     private String localName;
+
+    /**
+     * Treatment Protocool for the impression
+     */
     private String treatmentProtocol;
+
+    /**
+     * Remarks regarding the impression
+     */
     private String remarks;
+
+    /**
+     * Symptoms that are connected to the impression.
+     */
     private ArrayList<Symptom> symptoms;
 
+    /**
+     * Constructor.
+     * @param impressionId {@link #impressionId}
+     * @param impression {@link #impression}
+     * @param scientificName {@link #scientificName}
+     * @param localName {@link #localName}
+     * @param treatmentProtocol {@link #treatmentProtocol}
+     * @param remarks  {@link #remarks}
+     */
     public Impressions(int impressionId, String impression, String scientificName, String localName,
                        String treatmentProtocol, String remarks) {
         this.impressionId = impressionId;
@@ -25,47 +71,98 @@ public class Impressions {
         this.remarks = remarks;
     }
 
+    /**
+     * Gets {@link #impressionId}
+     * @return {@link #impressionId}
+     */
     public int getImpressionId() {
         return impressionId;
     }
 
+    /**
+     * Gets {@link #impression}
+     * @return {@link #impression}
+     */
     public String getImpression() {
         return impression;
     }
 
+
+    /**
+     * Sets {@link #impression}
+     * @param impression new value
+     */
     public void setImpression(String impression) {
         this.impression = impression;
     }
 
+    /**
+     * Gets {@link #symptoms}
+     * @return {@link #symptoms}
+     */
     public ArrayList<Symptom> getSymptoms() {
         return symptoms;
     }
 
+    /**
+     * Sets {@link #symptoms}
+     * @param symptoms new value
+     */
     public void setSymptoms(ArrayList<Symptom> symptoms) {
         this.symptoms = symptoms;
     }
 
+    /**
+     * Gets {@link #scientificName}
+     * @return {@link #impressionId}
+     */
     public String getScientificName() {
         return scientificName;
     }
 
+    /**
+     * Gets {@link #localName}
+     * @return {@link #localName}
+     */
     public String getLocalName() {
         return localName;
     }
 
+    /**
+     * Gets {@link #treatmentProtocol}
+     * @return {@link #treatmentProtocol}
+     */
     public String getTreatmentProtocol() {
         return treatmentProtocol;
     }
 
+    /**
+     * Gets {@link #remarks}
+     * @return {@link #remarks}
+     */
     public String getRemarks() {
         return remarks;
     }
+
+    /**
+     * Override method. Returns the hashCode.
+     *
+     * @see Object#hashCode()
+     */
 
     public int hashCode() {
 
         return impressionId;
     }
 
+    /**
+     * Override method.
+     * Objects are equal if impressionID are equal.
+     *
+     * @param object Object to be checked if equal
+     * @return true if the {@code object} and the Impression instance
+     * which called the method are equal.
+     */
     @Override
     public boolean equals(Object object) {
 
